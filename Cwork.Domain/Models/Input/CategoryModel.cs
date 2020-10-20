e814728d910394a150af8608fb779a3cf05d6c36
Prem <premverma7401@gmail.com>
2020-10-20 16:02:52 +1300

    Initial setup

---
+++ Cwork.Domain/Models/Input/CategoryModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cwork.Domain.Models.Input
+{
+  public class CategoryModel
+    {
+    }
+}
---
+++ Cwork.Persistance/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Cwork.Domain.Models.Input;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cwork.Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<CategoryModel> Categories { get; set; }
+    }
+}
